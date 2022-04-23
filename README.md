8b25004d7c9b7b8af97eebe76b3c6ac754110f35
Leiry Dhárya Ximenes <61087260+LeiryDharya@users.noreply.github.com>
2022-04-23 18:29:04 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# layout-site
-Layout de um site para uma empresa de produtos naturais
+# Flor do norte
+
+## Mobile para uma empresa de produtos naturais
+
+Como obter o projeto
+
+```sh
+git clone git@github.com:LeiryDharya/layout-mobile.git
+```
+
+Tecnologias utilizadas
+
+- HTML
+- CSS
+- Javascript
---
+++ main.js
@@ -0,0 +1,50 @@
+/*  abrir e fechar menu ao clicar no icone*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* ocultar menu após clicle em um item específico*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // scroll menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
+
+/*==ScrollReveal(mostrar elemento quando der scroll na página/contact==*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
